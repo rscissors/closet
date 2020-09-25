9210a726333b4a06e3ee33c27893a86c5acedd13
Rachel Scissors <rachelscissors@MacBook-Pro.fios-router.home>
2020-09-25 16:02:02 -0400

    initial xcode project creation

---
+++ closet/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  closet
+//
+//  Created by Rachel Scissors on 9/25/20.
+//  Copyright © 2020 Rachel Scissors. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ closet/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  closet
+//
+//  Created by Rachel Scissors on 9/25/20.
+//  Copyright © 2020 Rachel Scissors. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
